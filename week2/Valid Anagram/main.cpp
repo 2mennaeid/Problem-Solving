d2db02d3ca02811e6345e10774b9273725eae573
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-06 23:20:26 +0200

    Add files via upload

---
+++ week2/Valid Anagram/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isAnagram(string s, string t)
+{
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t)
+        {
+            return true;
+        }
+        else
+            return false;
+
+}
+int main()
+{
+
+    return 0;
+}
